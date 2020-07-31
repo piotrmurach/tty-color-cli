89928a05a3c4af2bfda044d202e2675a5695e216
Piotr Murach <pmurach@gmail.com>
2020-07-31 21:43:49 +0100

    Change to use system path

--- spec/unit/color_command_spec.rb
+++ spec/unit/color_command_spec.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.describe "color command" do
+  let(:cmd) { ::File.join("exe", "tty-color") }
+
   it "runs without arguments" do
-    expect(`exe/tty-color`).to match(/(true|false)/)
+    expect(`#{cmd}`).to match(/(true|false)/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "shows help" do
-    expect(`exe/tty-color --help`).to eq([
+    expect(`#{cmd} --help`).to eq([
       "Usage: tty-color [options]\n",
       "    -s, --support                    Check if terminal supports colors\n",
       "    -m, --mode                       Supported colors mode\n",
@@ -18,12 +20,12 @@ RSpec.describe "color command" do
   end
 
   it "detects mode" do
-    expect(`exe/tty-color --mode`).to match(/(\d+)/)
+    expect(`#{cmd} --mode`).to match(/(\d+)/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs in verbose mode" do
-    expect(`exe/tty-color --verbose --mode`).to match(/(\d+)/)
+    expect(`#{cmd} --verbose --mode`).to match(/(\d+)/)
     expect($?.exitstatus).to eq(0)
   end
 end
